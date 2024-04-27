1ffc2b8d41ff747b8d0e5ca5fb7a21c7eafc99dc
Joakim <jole@blackbird.online>
2024-04-27 14:48:34 +0200

    add createDragon endpoint and create default dragons for users

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,13 +9,40 @@ const port = 4000;
 const db = new DB();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.put("/user", (req, res) => {
-  res.send(db.createUser());
+  const user = db.createUser();
+
+  for (const name of ["Defaultious", "Dingus", "Bingus"]) {
+    db.createDragon(user, name);
+  }
+
+  res.send();
+});
+
+const idSchema = z.number().min(0, "Negative ID's should not exist");
+
+app.get("/user/:id", (req, res) => {
+  const id = idSchema.safeParse(+req.params.id);
+
+  if (id.error) {
+    res.status(401).send(id.error);
+    console.error("Failed with", id.error);
+    return;
+  }
+
+  const user = db.getUser(id.data);
+
+  if (user) {
+    res.send(user.toFrontend());
+  } else {
+    res.status(404).send("User not found");
+  }
 });
 
 // Dragons
@@ -25,10 +52,27 @@ const putDragonSchema = z.object({
     .string()
     .min(5, "Dragon name must be longer than 5 characters")
     .max(255, "Dragon name cannot be longer than 255 characters"),
+  userId: idSchema,
 });
 
 app.put("/dragon", (req, res) => {
-  const params = putDragonSchema.safeParse(req.params);
+  console.log("Raw is", req.body);
+
+  const params = putDragonSchema.safeParse(req.body);
+
+  if (params.error) {
+    res.status(401).send(params.error);
+    return;
+  }
+
+  const user = db.getUser(params.data.userId);
+
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
+  }
+
+  return res.send(db.createDragon(user, params.data.name).toFrontend());
 });
 
 app.listen(port, () => {
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,20 +3,53 @@ export class DB {
   private battles: Battle[] = [];
 
   public createUser(): User {
-    const user: User = { id: this.users.length, dragons: [] };
+    const user = new User(this.users.length, []);
     this.users.push(user);
     return user;
   }
+
+  public getUser(id: number): User | null {
+    return this.users[id] ?? null;
+  }
+
+  public createDragon(owner: User, name: string): Dragon {
+    const dragon = new Dragon(name, owner, null);
+    owner.dragons.push(dragon);
+    return dragon;
+  }
 }
 
-export type User = {
+export class User {
+  constructor(private id: number, public dragons: Dragon[]) {}
+
+  public toFrontend = (): FrontendUser => ({
+    id: this.id,
+    dragons: this.dragons.map((dragon) => dragon.toFrontend()),
+  });
+}
+
+export type FrontendUser = {
   id: number;
-  dragons: Dragon[];
+  dragons: FrontendDragon[];
 };
 
+let nextDragonId = 0;
+
 export class Dragon {
-  constructor(private name: string, private user: User) {}
+  private id: number;
+
+  constructor(
+    private name: string,
+    private owner: User,
+    private battle: Battle | null
+  ) {
+    this.id = nextDragonId++;
+  }
+
+  public toFrontend = (): FrontendDragon => ({ id: this.id, name: this.name });
 }
+
+export type FrontendDragon = { id: number; name: string };
 
 export class Battle {
   constructor(private dragons: [Dragon, Dragon]) {}
